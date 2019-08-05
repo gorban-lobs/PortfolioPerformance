4fe17024ced01bc7bd542fe82b2fb3f4977c838b
gor_lobs <nikita.lobosov.wa@yandex.ru>
2019-08-05 18:21:15 +0300

    Fix calculate_asset_performance and calculate_currency_performance methods

--- task.py
+++ task.py
@@ -17,7 +17,7 @@ class Portfolio:
 
     def _calc_assets_returns(self, curr_df):
         shifted_df = curr_df.shift(1)
-        return curr_df.sud(shifted_df).div(shifted_df)
+        return curr_df.sub(shifted_df).div(shifted_df)
 
     def _calc_weighted_returns(self, returns):
         if returns.columns.difference(self.weights.columns).empty:
@@ -31,24 +31,25 @@ class Portfolio:
         date_list = np.array([start_date])
         perf_list = np.array([1])
         for ind in range(1, len(series)):
-            np.append(date_list, series.index[ind])
-            np.append(perf_list, perf_list[ind - 1] * (1 + series.at[ind]))
+            date_list = np.append(date_list, series.index[ind])
+            perf_list = np.append(perf_list, perf_list[ind - 1] 
+                    * (1 + series.iat[ind]))
         return pd.Series(data=perf_list, index=date_list)
 
     def calculate_asset_performance(self, start_date, end_date):
-        asset_returns = _calc_asset_returns(self.prices)
-        weighted_return = asset_returns.mul(self.weights).sum(axis='columns')
-        return _calc_performance(weighted_return[start_date:end_date], 
+        asset_returns = self._calc_assets_returns(self.prices)
+        weighted_returns = self._calc_weighted_returns(asset_returns)
+        return self._calc_performance(weighted_returns[start_date:end_date],
                                 start_date)
 
     def calculate_currency_performance(self, start_date, end_date):
-        currency_returns = _calc_asset_returns(self.exchanges)
-        weighted_return = currency_returns.mul(self.weights).sum(axis='columns')
-        return _calc_performance(weighted_return[start_date:end_date], 
+        currency_returns = self._calc_assets_returns(self.exchanges)
+        weighted_returns = self._calc_weighted_returns(currency_returns)
+        return self._calc_performance(weighted_returns[start_date:end_date], 
                                 start_date)
 
     def calculate_total_performance(self, start_date, end_date):
-        total_returns = _calc_asset_returns(self.prices * self.currencies)
+        total_returns = _calc_assets_returns(self.prices * self.currencies)
         weighted_return = total_returns.mul(self.weights).sum(axis='columns')
         return _calc_performance(weighted_return[start_date:end_date],
                                 start_date)
