ab05cd48f5970fd8b0bf11279a5a40767dd11567
gor_lobs <nikita.lobosov.wa@yandex.ru>
2019-08-05 11:12:58 +0300

    Fix _count_return method, add division

--- task.py
+++ task.py
@@ -4,18 +4,11 @@ import pandas as pd
 
 class Portfolio:
     def __init__(self):
-        self.CURRENCIES_FILE = 'currencies.csv'
-        self.EXCHANGES_FILE = 'exchanges.csv'
-        self.PRICES_FILE = 'prices.csv'
-        self.WEIGHTS_FILE = 'weights.csv'
-        self.currencies = pd.read_csv(self.CURRENCIES_FILE, index_col=0)
-        self.exchanges = pd.read_csv(self.EXCHANGES_FILE, index_col=0))
-        self.prices = pd.read_csv(self.PRICES_FILE, index_col=0))
-        self.weights = pd.read_csv(self.WEIGHTS_FILE, index_col=0)
+        pass
 
     def _count_return(self, curr_df):
-        shifted_df = curr_df.shift(1, fill_value=0)
-        return curr_df.sub(shifted_df)
+        shifted_df = curr_df.shift(1)
+        return (curr_df - shifted_df) / shifted_df
 
     def calculate_asset_performance(self, start_date, end_date):
         pass
