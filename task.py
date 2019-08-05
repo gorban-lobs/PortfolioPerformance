b42a8ebd600c35949d51ced3d89ed66be916247b
gor_lobs <nikita.lobosov.wa@yandex.ru>
2019-08-05 12:13:55 +0300

    Rename _count_return to _calc_assets_returns; add calculation of weighted return

--- task.py
+++ task.py
@@ -3,21 +3,24 @@ import pandas as pd
 
 
 class Portfolio:
-    def __init__(self):
-        pass
+    def __init__(self, currencies, exchanges, prices, weights):
+        self.currencies = currencies
+        self.exchanges = exchanges
+        self.prices = prices
+        self.weights = weights
 
-    def _count_return(self, curr_df):
+    def _calc_assets_returns(self, curr_df):
         shifted_df = curr_df.shift(1)
         return (curr_df - shifted_df) / shifted_df
 
     def calculate_asset_performance(self, start_date, end_date):
-        pass
+        asset_returns = _calc_asset_returns(self.prices)
+        weighted_return = asset_returns.mul(self.weights).sum(axis='columns')
 
     def calculate_currency_performance(self, start_date, end_date):
-        pass
+        currency_returns = _calc_asset_returns(self.currencies)
+        weighted_return = currency_returns.mul(self.weights).sum(axis='columns')
 
     def calculate_total_performance(self, start_date, end_date):
-        pass
-
-
-pf = Portfolio()
+        total_returns = _calc_asset_returns(self.prices * self.currencies)
+        weighted_return = total_returns.mul(self.weights).sum(axis='columns')
