622a126262635604e089a1371557d82c876b1a74
gor_lobs <nikita.lobosov.wa@yandex.ru>
2019-08-06 13:06:12 +0300

    Add test_task.py module

---
+++ test_task.py
@@ -0,0 +1,14 @@
+import unittest
+import numpy as np
+import pandas as pd
+from task import Portfolio
+
+
+class TestPortfolio(unittest.TestCase):
+
+    def test__init_current_dataframes(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
